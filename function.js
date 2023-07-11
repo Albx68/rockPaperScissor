7d7ab0f69571f73f753f2780a4d874977496c139
Albert Yumnam <84399334+Albx68@users.noreply.github.com>
2023-07-11 16:26:10 +0530

    fixed function

--- function.js
+++ function.js
@@ -2,10 +2,10 @@ var button1 = document.getElementById('rock');
 var button2 = document.getElementById('paper');
 var button2 = document.getElementById('scissor');
 var user
-function.handleClick(user){
-   
+function handleClick(user) {
 
-   // var user = prompt("Enter your choice:"); !--> 
+
+    // var user = prompt("Enter your choice:"); !--> 
     var computer = ["rock", "paper", "scissor"];
     var computerAns = computer[Math.floor(Math.random() * computer.length)];
 
@@ -13,17 +13,17 @@ function.handleClick(user){
         rock: "scissor",
         paper: "rock",
         scissor: "paper"
-        };
+    };
 
     var result;
 
     if (user === computerAns) {
-         result = "It's a tie!";
-        } else if (choices[user] === computerAns) {
-            result = "You win!";
-        } else {
-            result = "You lose!";
-        }
+        result = "It's a tie!";
+    } else if (choices[user] === computerAns) {
+        result = "You win!";
+    } else {
+        result = "You lose!";
+    }
 
     //console.log(result);
     alert(result);
