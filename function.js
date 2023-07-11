a2602e12f355faf64947b5c3c559fa746f52f66e
jaskaram19 <81375036+jaskaram19@users.noreply.github.com>
2023-07-11 16:18:48 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,35 @@
+var button1 = document.getElementById('rock');
+var button2 = document.getElementById('paper');
+var button2 = document.getElementById('scissor');
+var user
+function.handleClick(user){
+   
+
+   // var user = prompt("Enter your choice:"); !--> 
+    var computer = ["rock", "paper", "scissor"];
+    var computerAns = computer[Math.floor(Math.random() * computer.length)];
+
+    var choices = {
+        rock: "scissor",
+        paper: "rock",
+        scissor: "paper"
+        };
+
+    var result;
+
+    if (user === computerAns) {
+         result = "It's a tie!";
+        } else if (choices[user] === computerAns) {
+            result = "You win!";
+        } else {
+            result = "You lose!";
+        }
+
+    //console.log(result);
+    alert(result);
+}
+
+// Add event listeners
+rock.addEventListener('click', handleClick(rock));
+paper.addEventListener('click', handleClick(paper));
+scissor.addEventListener('click', handleClick(paper));
